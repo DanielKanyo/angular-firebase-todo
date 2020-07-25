2f0f0e23c5d80ab587e703f0ac758fd36475709e
DanielKanyo <danielkanyo992@gmail.com>
2020-07-25 16:28:49 +0200

    Material design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { TodoItemComponent } from './todo-item/todo-item.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
     declarations: [AppComponent, TodoItemComponent],
-    imports: [BrowserModule],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        MatInputModule,
+        MatButtonModule,
+        MatIconModule,
+        MatCardModule,
+    ],
     providers: [],
     bootstrap: [AppComponent],
 })
